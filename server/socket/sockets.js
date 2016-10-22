6a9d0f71ac0e752ca58621fef3c99a742ef63e08
Michał Chatłas <micha.chatlas@gmail.com>
2016-10-22 14:12:48 +0200

    Dupa

--- server/socket/sockets.js
+++ server/socket/sockets.js
@@ -1,10 +1,11 @@
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-
-server.listen(1923);
-
 const setup = () => {
+
+    var app = require('express')();
+    var server = require('http').Server(app);
+    var io = require('socket.io')(server);
+
+    server.listen(1923);
+
     io.on('connection', function(socket){
         console.log('Tomasz Lis connected');
     });
