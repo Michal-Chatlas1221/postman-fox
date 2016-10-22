5feee9e19ad7748946a151037cf50a07ddbd94a6
Michał Chatłas <micha.chatlas@gmail.com>
2016-10-22 16:32:07 +0200

    pergaps finished server, perhaps not

--- server/socket/sockets.js
+++ server/socket/sockets.js
@@ -1,5 +1,12 @@
 const setup = () => {
 
+    const eventValues = [
+        {name: 'delivered', value: 100},
+        {name: 'crashed', value: -10},
+        {name: 'damaged', value: 50},
+        {name: 'picked', value: 5}
+    ];
+
     let publicState = {
         time: 0,
         gameTime: 10,
@@ -7,13 +14,17 @@ const setup = () => {
         timeUnit: 10
     };
 
+    let leaderBoard = [];
+
     var app = require('express')();
     var server = require('http').Server(app);
     var io = require('socket.io')(server);
+    var User = require('../models/user');
 
     server.listen(1923);
 
     io.on('connection', function(socket){
+
         socket.on('join', (data) => {
             console.log('asd', data);
             socket.broadcast.emit('connected', {
@@ -23,21 +34,47 @@ const setup = () => {
                 }
             });
         });
+
+        socket.on('gameEvent', (data) => {
+
+            let leaderBoardIndex = leaderBoard.findIndex(e => e.id === data.id);
+            if (leaderBoardIndex !== -1) {
+                leaderBoard[leaderBoardIndex].score += eventValues.find(e => e.name === data.name).value;
+            } else {
+                let user = User.findOne({_id: data.id});
+                leaderBoard.push({
+                    id: user._id,
+                    name: user.name,
+                    score: 0
+                });
+                leaderBoard[leaderBoard.length - 1].score += eventValues.find(e => e.name === data.name).value;
+            }
+
+            io.emit('scores', {
+                state: publicState,
+                eventSpecific: leaderBoard
+            })
+        });
     });
 
     setInterval(() => {
         publicState.time ++;
         if (publicState.time % (publicState.gameTime + publicState.pauseTime) === publicState.gameTime) {
-            io.emit('start', {
+            io.emit('stop', {
                 state: publicState
             });
+            io.emit('leaderBoard', {
+                state: publicState,
+                eventSpecific: leaderBoard
+            });
+            leaderBoard = [];
         } else if(publicState.time % (publicState.gameTime + publicState.pauseTime) === 0) {
-            io.emit('stop', {
+            io.emit('start', {
                 state: publicState
             });
         }
         publicState.time = publicState.time % (publicState.gameTime+publicState.pauseTime);
-    }, 10000);
+    }, 1000 * publicState.timeUnit);
 };
 
 module.exports = setup;
