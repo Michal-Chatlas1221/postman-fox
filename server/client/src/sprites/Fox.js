ec315645b25718e8c30273ba0cb85388e1ee5f38
Tomasz Lis <listmsz@gmail.com>
2016-10-25 16:37:15 +0200

    cleanup

--- server/client/src/sprites/Fox.js
+++ server/client/src/sprites/Fox.js
@@ -1,6 +1,6 @@
 import Phaser from 'phaser'
 
-export default class extends Phaser.Sprite {
+export default Fox class extends Phaser.Sprite {
 
     constructor ({ game, x, y, asset }) {
         super(game, x, y, asset);
